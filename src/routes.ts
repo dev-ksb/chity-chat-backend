66790a07e83a5205cf0471657c05b6ad90ef7ad3
kuldeep singh <devkuldeeps89@gmail.com>
2022-09-22 21:45:15 +0530

    setup initial project

---
+++ src/routes.ts
@@ -0,0 +1,8 @@
+import { Application } from 'express';
+
+export default (_app: Application) => {
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  const routes = () => {};
+
+  routes();
+};
---
+++ src/setupServer.ts
@@ -0,0 +1,121 @@
+import { Application, json, urlencoded, Response, Request, NextFunction } from 'express';
+import { createClient } from 'redis';
+import { createAdapter } from '@socket.io/redis-adapter';
+import { Server } from 'http';
+import { Server as SocketServer } from 'socket.io';
+
+import cors from 'cors';
+import compression from 'compression';
+import helmet from 'helmet';
+import hpp from 'hpp';
+import cookieSession from 'cookie-session';
+import HTTP_STATUS from 'http-status-codes';
+import 'express-async-errors';
+import Logger from 'bunyan';
+
+import applicationRoutes from './routes';
+import { config } from './config';
+import { CustomError, IErrorResponse } from './shared/globals/helpers/error-handler';
+
+const SERVER_PORT = 5000;
+const log: Logger = config.createLogger('server');
+
+export class ChattyServer {
+  private app: Application;
+
+  constructor(app: Application) {
+    this.app = app;
+  }
+
+  public start(): void {
+    this.securityMiddleware(this.app);
+    this.standardMiddleware(this.app);
+    this.routesMiddleware(this.app);
+    this.globalErrorMiddleware(this.app);
+    this.startServer(this.app);
+  }
+
+  private securityMiddleware(app: Application): void {
+    app.use(
+      cookieSession({
+        name: 'session',
+        keys: [config.SECRET_KEY_ONE!, config.SECRET_KEY_TWO!],
+        maxAge: 24 * 7 * 3600000,
+        secure: config.NODE_ENV !== 'development'
+      })
+    );
+
+    app.use(hpp());
+    app.use(helmet());
+
+    app.use(
+      cors({
+        origin: config.CLIENT_URL,
+        credentials: true,
+        optionsSuccessStatus: 200,
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
+      })
+    );
+  }
+
+  private standardMiddleware(app: Application): void {
+    app.use(compression());
+    app.use(json({ limit: '50mb' }));
+    app.use(urlencoded({ extended: true, limit: '50mb' }));
+  }
+
+  private routesMiddleware(app: Application): void {
+    applicationRoutes(app);
+  }
+
+  private globalErrorMiddleware(app: Application): void {
+    app.all('*', (req: Request, res: Response) => {
+      res.status(HTTP_STATUS.NOT_FOUND).json({ message: `${req.originalUrl} not found` });
+    });
+
+    app.use((error: IErrorResponse, _req: Request, res: Response, next: NextFunction) => {
+      log.error(error);
+      if (error instanceof CustomError) {
+        return res.status(error.statusCode).json(error.serializeErrors());
+      }
+
+      next();
+    });
+  }
+
+  private async startServer(app: Application): Promise<void> {
+    try {
+      const httpServer: Server = new Server(app);
+      const socketIO: SocketServer = await this.createSocketIO(httpServer);
+      this.startHttpServer(httpServer);
+      this.socketIOConnections(socketIO);
+    } catch (error) {
+      log.error(error);
+    }
+  }
+
+  private async createSocketIO(httpServer: Server): Promise<SocketServer> {
+    const io: SocketServer = new SocketServer(httpServer, {
+      cors: {
+        origin: config.CLIENT_URL,
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
+      }
+    });
+    const pubClient = createClient({ url: config.REDIS_HOST });
+    const subClient = pubClient.duplicate();
+    await Promise.all([pubClient.connect(), subClient.connect()]);
+    io.adapter(createAdapter(pubClient, subClient));
+    return io;
+  }
+
+  private startHttpServer(httpServer: Server): void {
+    log.info(`Server has started with process ${process.pid}`);
+
+    httpServer.listen(SERVER_PORT, () => {
+      log.info(`Server running on port ${SERVER_PORT}`);
+    });
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  private socketIOConnections(_io: SocketServer): void {}
+}
